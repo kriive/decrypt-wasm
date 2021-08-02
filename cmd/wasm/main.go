6f67d775eb50c7baadb1e5556c8b64521585d268
Manuel Romei <manuel.romei@raytecvision.com>
2021-08-02 18:48:17 +0200

    chore: add comments

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,3 +1,4 @@
+// cmd/wasm/main.go
 package main
 
 import (
@@ -9,11 +10,17 @@ import (
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 )
 
-func decryptPDF(input []uint8) ([]byte, error) {
+// DecryptPDF decrypts a PDF and returns the unencrypted file.
+// It uses pdfcpu, it's a bit of a hack, because I don't know
+// about API stability of the package and this could break in a update.
+// Who knows? \U0001f937
+func DecryptPDF(input []byte) ([]byte, error) {
 	r := bytes.NewReader(input)
 	var w bytes.Buffer
 
+	// Workaround: avoid pdfcpu to complain for missing config
 	pdfcpu.ConfigPath = "disable"
+
 	conf := pdfcpu.NewDefaultConfiguration()
 	conf.Cmd = pdfcpu.DECRYPT
 
@@ -36,25 +43,42 @@ func pdfWrapper() js.Func {
 			return "Invalid no of arguments passed"
 		}
 
+		// Outer function has one argument, which is the input locked PDF file
 		pdfBuf := args[0]
 
 		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			// This handler function will be wrapped inside a Promise, see below
+			// The first argument is a function called if everything's ok,
+			// the second one is a function called when something's gone wrong
 			resolve := args[0]
 			reject := args[1]
 
+			// Execute the decrypt function in a separate goroutine
 			go func() {
-				inBuf := make([]uint8, pdfBuf.Get("byteLength").Int())
+				inBuf := make([]byte, pdfBuf.Get("byteLength").Int())
 
+				// Copy the pdfBuf (containing the locked PDF as an array)
+				// to the Go WebAssembly runtime
 				js.CopyBytesToGo(inBuf, pdfBuf)
 
-				output, err := decryptPDF(inBuf)
+				// Do our little magic trick and call our decryption function
+				output, err := DecryptPDF(inBuf)
 				if err != nil {
+					// Create a new Error wrapping the error message
+					// returned by DecryptPDF
 					errorConstructor := js.Global().Get("Error")
 					errorObject := errorConstructor.New(err.Error())
+
+					// Invoke the sad path, calling the reject function
 					reject.Invoke(errorObject)
 				} else {
+					// Create a new Uint8Array with the proper length
 					dst := js.Global().Get("Uint8Array").New(len(output))
+
+					// Copy the decrypted output from Go runtime to JS
 					js.CopyBytesToJS(dst, output)
+
+					// We're done, call the happy path
 					resolve.Invoke(dst)
 				}
 			}()
@@ -62,7 +86,12 @@ func pdfWrapper() js.Func {
 			return nil
 		})
 
+		// Create the Promise
 		promiseConstructor := js.Global().Get("Promise")
+
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+		// A new Promise can be created from a function with two parameters
+		// In our case: resolve (happy path) and reject (sad path, an error occurred)
 		return promiseConstructor.New(handler)
 	})
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,8 @@
-module github.com/kriive/pdf-wass
+module github.com/kriive/pdf-wasm
 
 go 1.16
 
 require (
-	github.com/pdfcpu/pdfcpu v0.3.11
-	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/pdfcpu/pdfcpu v0.3.12
 	github.com/vearutop/statigz v1.1.4
 )
